e264fda43784977736fddcab11e53907e45721c2
wishmaster2016 <bliznyuk.sergei@gmail.com>
2013-12-17 00:26:59 +0200

    Revert "Revert "Init about, contact""
    This reverts commit 77fd00effe95f8cf52dcffed7dd37446a2da0bd0.

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@
 angular.module('test', ['ngRoute', 'test.filters', 'test.services', 'test.directives', 'test.controllers', 'ui.bootstrap', 'l10n', 'l10n-tools', 'test.languages']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/login.html', controller: 'loginCtrl'})
-                  .when('/404', {templateUrl: 'partials/404.html'})
+                  .when('/about', {templateUrl: 'partials/about.html', controller: 'aboutCtrl'})
+                  .when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactCtrl'})
+                  .when('/404', {templateUrl: 'partials/404.html', controller: '404Ctrl'})
                   .otherwise({redirectTo: '/404'});
   }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('test.controllers', []).
-  controller('loginCtrl', ['$scope', '$timeout', 'l10n', function($scope, $timeout, l10n) {
+angular.module('test.controllers', [])
+  .controller('loginCtrl', ['$scope', '$timeout', 'l10n', function($scope, $timeout, l10n) {
     l10n.setLocale('uk-UA');
     $scope.login = "";
     $scope.password = "";
@@ -24,4 +24,13 @@ angular.module('test.controllers', []).
       middle: true,
       right: false
     };*/
+  }])
+  .controller('aboutCtrl', ['$scope', 'l10n', function($scope, l10n) {
+
+  }])
+  .controller('contactCtrl', ['$scope', 'l10n', function($scope, l10n) {
+
+  }])
+  .controller('404Ctrl', ['$scope', 'l10n', function($scope, l10n) {
+
   }]);

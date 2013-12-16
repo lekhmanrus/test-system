77fd00effe95f8cf52dcffed7dd37446a2da0bd0
wishmaster2016 <bliznyuk.sergei@gmail.com>
2013-12-17 00:26:25 +0200

    Revert "Init about, contact"
    This reverts commit 585a7d13f125e1ceaa3501fb347f5261fd0bc606.

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,6 @@
 angular.module('test', ['ngRoute', 'test.filters', 'test.services', 'test.directives', 'test.controllers', 'ui.bootstrap', 'l10n', 'l10n-tools', 'test.languages']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/login.html', controller: 'loginCtrl'})
-                  .when('/about', {templateUrl: 'partials/about.html', controller: 'aboutCtrl'})
-                  .when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactCtrl'})
-                  .when('/404', {templateUrl: 'partials/404.html', controller: '404Ctrl'})
+                  .when('/404', {templateUrl: 'partials/404.html'})
                   .otherwise({redirectTo: '/404'});
   }]);

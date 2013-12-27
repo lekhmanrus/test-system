8e9fb8f13718cab31327e8dbe39f3aafc15e1713
ruslan_lekhman <lekhman-ruslan@i.ua>
2013-12-27 15:11:08 +0200

    Favicon fix

--- index.js
+++ index.js
@@ -4,36 +4,31 @@ var query = require('./js/query.js');
 var express = require('express');
 var app = express();
 
-app.use(express.json());
-
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/public/index.html');
-});
-
-app.get('/data', function(req, res) {
-  query("SELECT login FROM users WHERE id = 1", function(data) {
-    res.json(data);
-  });
-});
-
-app.get('/login/:login/:password', function(req, res) {
-  query("SELECT * FROM users WHERE login = '" + req.params.login + "' AND password = '" + req.params.password + "'", function(data) {
-    res.json(data);
-  });
-});
-
-app.post('/register', function(req, res) {
-  query("SELECT * FROM users WHERE login = '" + req.body.login + "' AND password = '" + req.body.password + "'", function(data) {
-    if(data.length != 0) {
-      res.json({error : 1});
-      return;
-    }
-    query("INSERT INTO users (login, password, email, name, surname, patronymic) VALUES ('" + req.body.login + "', '" + req.body.password + "', '" + req.body.email + "', '" + req.body.name + "', '" + req.body.surname + "', '" + req.body.patronymic + "');", function(data) {
-      res.json({success : 1});
+app.use(express.json())
+  .use(express.favicon(__dirname + '/public/images/favicon.ico'))
+  .get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+  })
+  .get('/data', function(req, res) {
+    query("SELECT login FROM users WHERE id = 1", function(data) {
+      res.json(data);
     });
-  });
-});
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(process.env.PORT || 8888);
+  })
+  .get('/login/:login/:password', function(req, res) {
+    query("SELECT * FROM users WHERE login = '" + req.params.login + "' AND password = '" + req.params.password + "'", function(data) {
+      res.json(data);
+    });
+  })
+  .post('/register', function(req, res) {
+    query("SELECT * FROM users WHERE login = '" + req.body.login + "' AND password = '" + req.body.password + "'", function(data) {
+      if(data.length != 0) {
+        res.json({error : 1});
+        return;
+      }
+      query("INSERT INTO users (login, password, email, name, surname, patronymic) VALUES ('" + req.body.login + "', '" + req.body.password + "', '" + req.body.email + "', '" + req.body.name + "', '" + req.body.surname + "', '" + req.body.patronymic + "');", function(data) {
+        res.json({success : 1});
+      });
+    });
+  })
+  .use(express.static(__dirname + '/public'))
+  .listen(process.env.PORT || 8888);
